c2a2b89e6d012e50bd8423fc101c12987dd6fc53
Gsocram <202507287192@alunos.estacio.br>
2025-10-01 22:12:42 +0000

    novo printf

--- xadrez.c
+++ xadrez.c
@@ -1,52 +1,101 @@
 #include <stdio.h>
 
-int main() {
+#define NUM_CASAS_MOVER 4 
 
-
-printf("Desafio de Xadrez Novato movimentação das peças\n");
-
-    printf("---Movimento do Bispo (2 casas na diagonal)---\n");
-    int casas_bispo = 1;
-    do{
-        printf("Cima, Direita: %d\n", casas_bispo);
-        casas_bispo++;
-    }while(casas_bispo <2);
-
-    printf("\n");
-
-    printf("--- Movimento da Torre (5 casas para a direita)---\n");
-    for(int i = 1; i <= 5; i++){
-        printf("Direita: %d\n", i);
+void moverTorreRecursivo(int casasRestantes) {
+    if (casasRestantes <= 0) {
+        return;
     }
-    printf("\n");
+    printf("Direita\n");
+    moverTorreRecursivo(casasRestantes - 1); 
+}
 
-    printf("--- Movimento da Rainha (8 casas para a esquerda)---\n");
-    int casas_rainha = 1;
-    while (casas_rainha <= 8){
-        printf("Esquerda: %d\n", casas_rainha);
-        casas_rainha++;
+void moverRainhaRecursivo(int casasRestantes) {
+    if (casasRestantes <= 0) {
+        return;
     }
-    printf("\n");
-    printf("--- Movimento do Cavalo (Duas para baixo e uma para a esquerda) ---\n");
+    printf("Cima\n");
+    printf("Direita\n");
+    moverRainhaRecursivo(casasRestantes - 1);
+}
 
-    printf("Fase 1 (Vertical) - Duas casas para baixo:\n");
-    for (int i = 1; i <= 2; i++) {
-        printf("Baixo: %d\n", i);
-        if (i == 2) {
-            printf("  (Fim da fase vertical, iniciando a fase horizontal)\n");
-            int j = 1;
-        
-            while (j <= 1) {
-                printf("Esquerda: %d (Completa o L)\n", j);
-                j++;
+void moverBispoRecursivo(int casasRestantes) {
+    if (casasRestantes <= 0) {
+        return;
+    }
+    printf("Cima\n");
+    printf("Esquerda\n");
+    moverBispoRecursivo(casasRestantes - 1);
+}
+
+void moverCavaloComplexo() {
+    int i; 
+    int passos_verticais = 2;
+    int passos_horizontais = 1;
+
+    for (i = 1; i <= passos_verticais; i++) {
+        printf("Cima\n");
+
+        if (i < passos_verticais) {
+            continue; 
+        }
+
+        int j; 
+        for (j = 1; j <= passos_horizontais; j++) {
+            printf("Direita\n");
+            
+            if (j == passos_horizontais) {
+                break; 
             }
         }
+        
+        if (i == passos_verticais) {
+            break; 
+        }
     }
+}
+
+void moverBispoAninhado() {
+    int i; 
+    int max_movimentos = NUM_CASAS_MOVER;
+
+    for (i = 1; i <= max_movimentos; i++) {
+        printf("Baixo\n");
+
+        int j;
+        for (j = 1; j <= 1; j++) {
+            printf("Esquerda\n");
+        }
+    }
+}
+
+int main() {
+    printf("--- SIMULACAO DE MOVIMENTOS DE PEÇAS DE XADREZ (AVANÇADO) ---\n\n");
+    
+    // TORRE com RECURSIVIDADE
+    printf("--- Movimento da TORRE (Recursivo - %d casas Direita) ---\n", NUM_CASAS_MOVER);
+    moverTorreRecursivo(NUM_CASAS_MOVER);
     printf("\n");
 
+    // BISPO com RECURSIVIDADE
+    printf("--- Movimento do BISPO (Recursivo - %d casas Diagonal Cima/Esquerda) ---\n", NUM_CASAS_MOVER);
+    moverBispoRecursivo(NUM_CASAS_MOVER);
+    printf("\n");
+    
+    // RAINHA com RECURSIVIDADE
+    printf("--- Movimento da RAINHA (Recursivo - %d casas Diagonal Cima/Direita) ---\n", NUM_CASAS_MOVER);
+    moverRainhaRecursivo(NUM_CASAS_MOVER);
+    printf("\n");
 
+    // CAVALO com LOOPS COMPLEXOS
+    printf("--- Movimento do CAVALO (Loops Complexos - 2 Cima, 1 Direita) ---\n");
+    moverCavaloComplexo();
+    printf("\n");
 
-
+    // BISPO com LOOPS ANINHADOS
+    printf("--- Movimento do BISPO (Loops Aninhados - %d casas Diagonal Baixo/Esquerda) ---\n", NUM_CASAS_MOVER);
+    moverBispoAninhado();
+    printf("\n");
 
     return 0;
 }
